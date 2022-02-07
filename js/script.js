f4eda676651fff0b8bc09ead808a57850a10d531
Kim Do Hee <95347166+doright84@users.noreply.github.com>
2022-02-08 00:12:39 +0900

    포트폴리오 프로세스 1차

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    AOS.init();
+    // ---gotop----
+    $('.gotop').click(function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        });
+    });
+    $(".gotop").hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 400) {
+            $('.gotop').slideDown();
+
+        } else {
+            $('.gotop').slideUp();
+
+        }
+
+    });
+
+
+ 
+});
